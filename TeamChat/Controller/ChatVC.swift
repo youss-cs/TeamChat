86b592aba9de74dc752d9d1e857c7c48a633b840
YouSS <youss.cs@gmail.com>
2018-09-30 16:41:40 +0100

    Left menu added

---
+++ TeamChat/Controller/ChatVC.swift
@@ -0,0 +1,31 @@
+//
+//  ChatVC.swift
+//  TeamChat
+//
+//  Created by YouSS on 9/30/18.
+//  Copyright © 2018 YouSS. All rights reserved.
+//
+
+import UIKit
+import SideMenu
+
+class ChatVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        SideMenuManager.default.menuFadeStatusBar = false
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
